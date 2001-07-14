1c8cd33a91dd30cf767bbf5b2b5c31252e1743dd
Juergen Keil <jkeil@users.sourceforge.net>
2001-07-14 19:13:11 +0100

    timeradd/timersub is missing on solaris, provide some macros to implement these functions.

--- src/xitk/xine-toolkit/browser.c
+++ src/xitk/xine-toolkit/browser.c
@@ -39,10 +39,42 @@
 
 #include "_xitk.h"
 
+
+/*
+ * timeradd/timersub is missing on solaris' sys/time.h, provide
+ * some fallback macros
+ */
+#ifndef	timeradd
+#define timeradd(a, b, result)                                                \
+  do {                                                                        \
+    (result)->tv_sec = (a)->tv_sec + (b)->tv_sec;                             \
+    (result)->tv_usec = (a)->tv_usec + (b)->tv_usec;                          \
+    if ((result)->tv_usec >= 1000000)                                         \
+      {                                                                       \
+        ++(result)->tv_sec;                                                   \
+        (result)->tv_usec -= 1000000;                                         \
+      }                                                                       \
+  } while (0)
+#endif
+
+#ifndef timersub
+#define timersub(a, b, result)                                                \
+  do {                                                                        \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;                             \
+    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;                          \
+    if ((result)->tv_usec < 0) {                                              \
+      --(result)->tv_sec;                                                     \
+      (result)->tv_usec += 1000000;                                           \
+    }                                                                         \
+  } while (0)
+#endif
+
+
 typedef struct {
   widget_t    *itemlist;
   int          sel;
 } btnlist_t;
+
 
 #define WBUP    0  /*  Position of button up in item_tree  */
 #define WSLID   1  /*  Position of slider in item_tree  */
