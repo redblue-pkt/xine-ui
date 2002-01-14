62fd842ed88077e305d203ab29986e36d47a073a
Siggi Langauf <siggi@users.sourceforge.net>
2002-01-14 01:11:28 +0000

    added xine-check (and xine-bugreport) removed the (unused??) snapshot script from Makefile.am

---
+++ misc/build-xine-check.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+#
+# build-xine-check.pl - build xine-check from .templ and message files
+# 
+# usage: build-xine-check.pl <message file> #FIXME: support several msgfiles
+
+$templ="xine-check.sh.in";
+$out="xine-check";
+
+open TEMPLATE, $templ || die "unable to open input file";
+open OUT, ">$out" || die "unable to open output file";
+
+$insert='cat <<EOF >/tmp/xck.$$.xine-check.en'."\n";
+while (<>) {
+  s/\$/\\\$/g;
+  $insert .= $_;
+}
+$insert .= "EOF\n";
+
+while (<TEMPLATE>) {
+  if (/^\#\# auto-insert-language-files-here \#\#/) {
+    print OUT $insert;
+  }
+  else {
+    print OUT $_;
+  }
+}
