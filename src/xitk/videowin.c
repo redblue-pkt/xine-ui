9d44f58b7caa6a38652627faa38b09a0a4f356d9
Matthias Dahl <matt2000@users.sourceforge.net>
2002-04-28 20:54:57 +0100

    Fixing "SyncFB overlay out of the video window at startup" bug.

--- src/xitk/videowin.c
+++ src/xitk/videowin.c
@@ -909,6 +909,14 @@ void video_window_init (void) {
 
   /* Ensure we load the right logo */
   video_window_change_skins();
+
+  // for plugins that aren't really bind to the window, it's necessary that the
+  // gVw->xwin and gVw->ywin variables are set to right values, otherwise the
+  // overlay will be displayed somewhere outside the window
+  if(gGui->video_window) {
+     Window tmp_win;
+     XTranslateCoordinates(gGui->display, gGui->video_window, DefaultRootWindow(gGui->display), 0, 0, &gVw->xwin, &gVw->ywin, &tmp_win);
+  }
 }
 
 
