36973ceba3833197595e871446586306f5eb3269
Daniel Caujolle-Bert <f1rmb@users.sourceforge.net>
2002-06-23 11:14:17 +0100

    Fix segfault on IA64 patch from David Mosberger-Tang <David.Mosberger@acm.org>.

--- src/xitk/main.c
+++ src/xitk/main.c
@@ -619,8 +619,8 @@ static void codec_reporting(void *user_data, int codec_type,
  * initialize codec reporting stuff.
  */
 static void init_report_codec(void) {
-  char  *warn_unhandled_codec[5];
-
+  static char *warn_unhandled_codec[5];
+  
   warn_unhandled_codec[0] = _("never");
   warn_unhandled_codec[1] = _("video only");
   warn_unhandled_codec[2] = _("audio only");
