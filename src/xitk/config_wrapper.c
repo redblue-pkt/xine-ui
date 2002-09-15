96198cf6df02d931401ef5a8439dd014e9ef320a
Michael Roitzsch <mroi@users.sourceforge.net>
2002-09-15 17:35:49 +0100

    the lookup function has C boolean semantics, not shell semantics

--- src/xitk/config_wrapper.c
+++ src/xitk/config_wrapper.c
@@ -66,7 +66,7 @@ void config_update_range(char *key, int min, int max) {
   xine_cfg_entry_t entry;
 
   memset(&entry, 0, sizeof(xine_cfg_entry_t));
-  if(xine_config_lookup_entry(gGui->xine, key, &entry)==0) 
+  if(xine_config_lookup_entry(gGui->xine, key, &entry)) 
     config_update(&entry, XINE_CONFIG_TYPE_RANGE, min, max, 0, NULL);
   else
     fprintf(stderr, "WOW, key %s isn't registered\n", key);
@@ -77,7 +77,7 @@ void config_update_string(char *key, char *string) {
 
   memset(&entry, 0, sizeof(xine_cfg_entry_t));
   
-  if((xine_config_lookup_entry(gGui->xine, key, &entry)==0) && string)
+  if((xine_config_lookup_entry(gGui->xine, key, &entry)) && string)
     config_update(&entry, XINE_CONFIG_TYPE_STRING, 0, 0, 0, string);
   else {
     if(string == NULL)
@@ -91,7 +91,7 @@ void config_update_enum(char *key, int value) {
   xine_cfg_entry_t entry;
 
   memset(&entry, 0, sizeof(xine_cfg_entry_t));
-  if(xine_config_lookup_entry(gGui->xine, key, &entry)==0) 
+  if(xine_config_lookup_entry(gGui->xine, key, &entry)) 
     config_update(&entry, XINE_CONFIG_TYPE_ENUM, 0, 0, value, NULL);
   else
     fprintf(stderr, "WOW, key %s isn't registered\n", key);
@@ -101,7 +101,7 @@ void config_update_bool(char *key, int value) {
   xine_cfg_entry_t entry;
 
   memset(&entry, 0, sizeof(xine_cfg_entry_t));
-  if(xine_config_lookup_entry(gGui->xine, key, &entry)==0) 
+  if(xine_config_lookup_entry(gGui->xine, key, &entry)) 
     config_update(&entry, XINE_CONFIG_TYPE_BOOL, 0, 0, ((value > 0) ? 1 : 0), NULL);
   else
     fprintf(stderr, "WOW, key %s isn't registered\n", key);
@@ -111,7 +111,7 @@ void config_update_num(char *key, int value) {
   xine_cfg_entry_t entry;
 
   memset(&entry, 0, sizeof(xine_cfg_entry_t));
-  if(xine_config_lookup_entry(gGui->xine, key, &entry)==0) 
+  if(xine_config_lookup_entry(gGui->xine, key, &entry)) 
     config_update(&entry, XINE_CONFIG_TYPE_NUM, 0, 0, value, NULL);
   else
     fprintf(stderr, "WOW, key %s isn't registered\n", key);
