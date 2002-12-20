d3b4a283dfbe221f5af56b811417b975b78b0fb6
Robin KAY <komadori@users.sourceforge.net>
2002-12-20 21:05:01 +0000

    Fix incorrect names in the autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -28,7 +28,7 @@ detect_configure_ac() {
 detect_autoconf() {
   (autoconf --version) < /dev/null > /dev/null 2>&1 || {
     echo
-    echo "**Error**: You must have \`autoconf' installed to compile gxine."
+    echo "**Error**: You must have \`autoconf' installed to compile xine-ui."
     echo "Download the appropriate package for your distribution,"
     echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
     exit 1
