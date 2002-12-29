346396176306e9c779c4e6586777471c565418a0
Daniel Caujolle-Bert <f1rmb@users.sourceforge.net>
2002-12-29 17:48:51 +0000

    remove unneeded printf(s)

--- src/xitk/xine-toolkit/mrlbrowser.c
+++ src/xitk/xine-toolkit/mrlbrowser.c
@@ -885,17 +885,12 @@ static void handle_dbl_click(xitk_widget_t *w, void *data, int selected) {
   
   xitk_get_key_modifier(&xev, &modifier);
   
-  if((modifier & MODIFIER_CTRL) && (modifier & MODIFIER_SHIFT)) {
-    printf("CTRL && SHIFT\n");
+  if((modifier & MODIFIER_CTRL) && (modifier & MODIFIER_SHIFT))
     mrlbrowser_select_mrl(private_data, selected, 1, 1);
-  }
-  else if(modifier & MODIFIER_CTRL) {
-    printf("CTRL\n");
+  else if(modifier & MODIFIER_CTRL)
     mrlbrowser_select_mrl(private_data, selected, 1, 0);
-  }
-  else {
+  else
     mrlbrowser_select_mrl(private_data, selected, 0, 1); 
-  }
 
 }
 
