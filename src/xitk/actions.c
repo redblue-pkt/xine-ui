56f2ae45f0c8bf69ba849bad889d377c3f1b4744
Daniel Caujolle-Bert <f1rmb@users.sourceforge.net>
2002-12-31 13:10:58 +0000

    Fix gui_play() and mrl browser _just_to_play_ feature.

--- src/xitk/actions.c
+++ src/xitk/actions.c
@@ -91,7 +91,8 @@ int gui_xine_play(xine_stream_t *stream, int start_pos, int start_time_in_secs, 
   
   if((has_video || gGui->visual_anim.enabled == 1) && gGui->visual_anim.running) {
     xine_post_out_t * audio_source;
-    
+
+    printf("CASE 1.\n");
     audio_source = xine_get_audio_source(stream);
     if(xine_post_wire_audio_port(audio_source, gGui->ao_port)) {
       if(gGui->visual_anim.enabled == 1)
@@ -102,6 +103,7 @@ int gui_xine_play(xine_stream_t *stream, int start_pos, int start_time_in_secs, 
 	     (gGui->visual_anim.running == 0) && gGui->visual_anim.post_output) {
     xine_post_out_t * audio_source;
 
+    printf("CASE 2.\n");
     audio_source = xine_get_audio_source(stream);
     if(xine_post_wire_audio_port(audio_source, gGui->visual_anim.post_output->audio_input[0]))
       gGui->visual_anim.running = 1;
@@ -110,6 +112,7 @@ int gui_xine_play(xine_stream_t *stream, int start_pos, int start_time_in_secs, 
   else if(has_video && gGui->post_video && (gGui->post_video_num > 0)) {
     xine_post_out_t *video_source;
     
+    printf("CASE 3.\n");
     video_source = xine_get_video_source(stream);
     if(xine_post_wire_video_port(video_source, gGui->post_video->video_input[0]))
       printf("xine_post_wire_video_port() succeed\n");
@@ -367,7 +370,7 @@ void gui_exit (xitk_widget_t *w, void *data) {
 
 void gui_play (xitk_widget_t *w, void *data) {
 
-  if(!gGui->playlist.num)
+  if((!gGui->playlist.num) && (xine_get_status(gGui->stream) != XINE_STATUS_PLAY))
     return;
 
   video_window_reset_ssaver();
