ca55bfb1dad253b1219d5dca73bdc609de8ca80e
Daniel Caujolle-Bert <f1rmb@users.sourceforge.net>
2003-01-02 21:32:07 +0000

    remove unneeded printf

--- src/xitk/main.c
+++ src/xitk/main.c
@@ -1253,7 +1253,7 @@ int main(int argc, char *argv[]) {
     xine_cfg_entry_t  cfg_vo_entry;
     
     if(xine_config_lookup_entry(gGui->xine, "video.driver", &cfg_vo_entry)) {
-      printf("xine_config_lookup_entry() succeed: '%s'\n", cfg_vo_entry.str_value);
+
       if(!strcasecmp(video_driver_ids[cfg_vo_entry.num_value], "dxr3")) {
 	xine_cfg_entry_t  cfg_entry;
 	
