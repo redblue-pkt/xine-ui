820fb759c26176b1eb27096903070782d49d0617
Daniel Caujolle-Bert <f1rmb@users.sourceforge.net>
2003-01-19 00:31:30 +0000

    turn off one warning

--- src/xitk/videowin.c
+++ src/xitk/videowin.c
@@ -1628,7 +1628,7 @@ void video_window_update_logo(void) {
 	}
 	if((!xine_open(gGui->stream, gGui->logo_mrl)) 
 	   || (!xine_play(gGui->stream, 0, 0))) {
-	  gui_handle_xine_error(gGui->stream, gGui->logo_mrl);
+	  gui_handle_xine_error(gGui->stream, (char *)gGui->logo_mrl);
 	  goto __done;
 	}
 	gGui->logo_mode = 1;
