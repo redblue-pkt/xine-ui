18cc5d3722dfb37119ec0b546e0b82c8daae48be
Daniel Caujolle-Bert <f1rmb@users.sourceforge.net>
2003-04-13 01:50:15 +0100

    update xitk version

--- src/xitk/xine-toolkit/xitk.h
+++ src/xitk/xine-toolkit/xitk.h
@@ -43,8 +43,8 @@
 
 #define XITK_MAJOR_VERSION          (0)
 #define XITK_MINOR_VERSION          (10)
-#define XITK_SUB_VERSION            (2)
-#define XITK_VERSION                "0.10.2"
+#define XITK_SUB_VERSION            (3)
+#define XITK_VERSION                "0.10.3"
 
 #define XITK_CHECK_VERSION(major, minor, sub)                          \
                                     (XITK_MAJOR_VERSION > (major) ||   \
