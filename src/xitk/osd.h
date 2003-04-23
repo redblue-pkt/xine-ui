0ad5447afaafa8582b43a305aa542f6a5399f935
Daniel Caujolle-Bert <f1rmb@users.sourceforge.net>
2003-04-23 23:00:54 +0100

    oops, wrong proto

--- src/xitk/osd.h
+++ src/xitk/osd.h
@@ -35,7 +35,7 @@ void osd_init(void);
 void osd_deinit(void);
 void osd_update(void);
 void osd_display_spu_lang(void);
-void osd_display_lang_lang(void);
+void osd_display_audio_lang(void);
 void osd_stream_infos(void);
 void osd_update_status(void);
 void osd_stream_position(int pos);
