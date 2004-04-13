352c9d924b6e519b5fc051954a5db164a28caa1a
Daniel Caujolle-Bert <f1rmb@users.sourceforge.net>
2004-04-13 20:55:51 +0100

    update year

--- src/xitk/network.c
+++ src/xitk/network.c
@@ -1,6 +1,6 @@
 #warning IMPLEMENT POST SUPPORT
 /*
- * Copyright (C) 2000-2003 the xine project
+ * Copyright (C) 2000-2004 the xine project
  * 
  * This file is part of xine, a unix video player.
  * 
