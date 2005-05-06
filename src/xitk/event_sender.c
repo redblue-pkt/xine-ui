fe5bc95929ede140af3de0710b2bc498b18a1278
Hans-Dieter Kosch <fsck-p@users.sourceforge.net>
2005-05-07 00:32:33 +0100

    Satisfied gcc complaining about "nested extern declaration". It was my fault, introduced in previous version.

--- src/xitk/event_sender.c
+++ src/xitk/event_sender.c
@@ -212,6 +212,8 @@ static void event_sender_angle(xitk_widget_t *w, void *data) {
   event_sender_send(events[event]);
 }
 
+static void event_sender_exit(xitk_widget_t *, void *);
+
 static void event_sender_handle_event(XEvent *event, void *data) {
   
   switch(event->type) {
@@ -285,11 +287,8 @@ static void event_sender_handle_event(XEvent *event, void *data) {
 	event_sender_num(NULL, (void *)10);
 	break;
 
-      case XK_Escape: {
-	  static void event_sender_exit(xitk_widget_t *, void *);
-
-	  event_sender_exit(NULL, NULL);
-	}
+      case XK_Escape:
+	event_sender_exit(NULL, NULL);
 	break;
 
       default:
