14c57888f9b05871f18fb5a69cf163c72bda6c27
Ryan Schmidt <xine-project-2009@ryandesign.com>
2009-06-15 13:03:00 +0000

    INADDR_NONE undeclared in network.c on Mac OS X

--- src/xitk/network.c
+++ src/xitk/network.c
@@ -26,7 +26,7 @@
 /* required for getsubopt(); the __sun test avoids compilation problems on 
     solaris. On FreeBSD defining this disable BSD functions to be visible
     and remove INADDR_NONE */
-#if ! defined (__sun__) && ! defined (__OpenBSD__)  && ! defined(__FreeBSD__)
+#if ! defined (__sun__) && ! defined (__OpenBSD__)  && ! defined(__FreeBSD__) && ! defined(__APPLE__)
 #define _XOPEN_SOURCE 500
 #endif
 /* required for strncasecmp() */
