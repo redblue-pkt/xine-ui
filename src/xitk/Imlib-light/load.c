ad80d1a20b17bdac7da9bb53d5547e2ed858b617
Thomas Klausner <tk@giga.or.at>
2011-01-24 15:12:25 +0000

    Fix build with png-1.5.

--- src/xitk/Imlib-light/load.c
+++ src/xitk/Imlib-light/load.c
@@ -61,7 +61,7 @@ unsigned char *_LoadPNG(ImlibData * id, FILE * f, int *w, int *h, int *t) {
       png_destroy_read_struct(&png_ptr, NULL, NULL);
       return NULL;
     }
-  if (info_ptr->color_type == PNG_COLOR_TYPE_RGB_ALPHA)
+  if (png_get_color_type(png_ptr, info_ptr) == PNG_COLOR_TYPE_RGB_ALPHA)
     {
       png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
       return NULL;
