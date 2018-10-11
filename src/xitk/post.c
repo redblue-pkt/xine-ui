a56cdff1b0ae100c5d195e0b9ff6d2144caaac11
Petri Hintukainen <phintuka@users.sourceforge.net>
2018-10-11 17:39:53 +0300

    add const

--- src/xitk/post.c
+++ src/xitk/post.c
@@ -66,9 +66,9 @@
  
 typedef struct {
   xine_post_t                 *post;
-  xine_post_api_t             *api;
-  xine_post_api_descr_t       *descr;
-  xine_post_api_parameter_t   *param;
+  const xine_post_api_t             *api;
+  const xine_post_api_descr_t       *descr;
+  const xine_post_api_parameter_t   *param;
   char                        *param_data;
 
   int                          x;
@@ -887,12 +887,12 @@ static int __pplugin_retrieve_parameters(post_object_t *pobj) {
   xine_post_in_t             *input_api;
   
   if((input_api = (xine_post_in_t *) xine_post_input(pobj->post, "parameters"))) {
-    xine_post_api_t            *post_api;
-    xine_post_api_descr_t      *api_descr;
-    xine_post_api_parameter_t  *parm;
+    const xine_post_api_t            *post_api;
+    const xine_post_api_descr_t      *api_descr;
+    const xine_post_api_parameter_t  *parm;
     int                         pnum = 0;
     
-    post_api = (xine_post_api_t *) input_api->data;
+    post_api = (const xine_post_api_t *) input_api->data;
     
     api_descr = post_api->get_param_descr();
     
