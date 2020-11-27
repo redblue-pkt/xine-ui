5e06688a123e3d622cebb781546c98f9330a37e5
Petri Hintukainen <phintuka@users.sourceforge.net>
2020-11-27 21:44:22 +0200

    ImLib: drop unused lock functions

--- src/xitk/Imlib-light/Imlib_types.h
+++ src/xitk/Imlib-light/Imlib_types.h
@@ -80,8 +80,6 @@ struct _ImlibImage
 struct _xdata
   {
     Display            *disp;
-    void              (*x_lock_display) (Display *display);
-    void              (*x_unlock_display) (Display *display);
     int                 screen;
     Window              root;
     Visual             *visual;
